bae4e47a807f6f7549faab62e66889efd8a47a98
cubasve <eva.cubasvasquez@gmail.com>
2021-05-11 23:32:23 -0400

    Add icons and title of content and round the pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 import './App.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+  faSyncAlt, 
+  faHeart, 
+  faFistRaised, 
+  faPaw, 
+  faClock, 
+  faSmile, 
+  faGrinTears, 
+  faChartLine, 
+  faRunning, 
+  faBrain } from '@fortawesome/free-solid-svg-icons';
+
 export default function App() {
 
   useEffect(() => {
@@ -33,29 +46,34 @@ export default function App() {
   }, []);
 
   return (
-    <div>
+    // <div>
 
-      <main>
-        <div id="book-animation">
+    //   <main>
+        <main id="book-animation">
           <div className="scroll book">
             <h1>Intro</h1>
           </div>
 
-          {/* <div className="book" /> */}
+          <div className="book" />
 
           <div className="book" id="book">
 
             {/* 1st Set */}
-            <div className="page book cover front book-page">
+            {/* <div className="page book cover front book-page"> */}
+            <div className="page book cover book-page">
               <div className="page half front">
                 <div className="content">
                   <h1>Page 1</h1>
+                  <FontAwesomeIcon icon={faSyncAlt} size="3x" />
+                  <h3>Today I begin a new life</h3>
                 </div>
               </div>
 
               <div className="page half back">
                 <div className="content">
                   <h1>Page 2</h1>
+                  <FontAwesomeIcon icon={faHeart} size="3x" />
+                  <h3>I will greet this day with love in my heart</h3>
                 </div>
               </div>
             </div>
@@ -65,12 +83,16 @@ export default function App() {
               <div className="page half front">
                 <div className="content">
                   <h1>Page 3</h1>
+                  <FontAwesomeIcon icon={faFistRaised} size="3x" />
+                  <h3>I will persist until I succeed</h3>
                 </div>
               </div>
 
               <div className="page half back">
                 <div className="content">
                   <h1>Page 4</h1>
+                  <FontAwesomeIcon icon={faPaw} size="3x" />
+                  <h3>I am nature's greatest miracle</h3>
                 </div>
               </div>
             </div>
@@ -80,12 +102,16 @@ export default function App() {
               <div className="page half front">
                 <div className="content">
                   <h1>Page 5</h1>
+                  <FontAwesomeIcon icon={faClock} size="3x" />
+                  <h3>I will live this day as if it is my last</h3>
                 </div>
               </div>
 
               <div className="page half back">
                 <div className="content">
                   <h1>Page 6</h1>
+                  <FontAwesomeIcon icon={faSmile} size="3x" />
+                  <h3>I will be the master of my emotions</h3>
                 </div>
               </div>
             </div>
@@ -95,12 +121,16 @@ export default function App() {
               <div className="page half front">
                 <div className="content">
                   <h1>Page 7</h1>
+                  <FontAwesomeIcon icon={faGrinTears} size="3x" />
+                  <h3>I will laugh at the world</h3>
                 </div>
               </div>
 
               <div className="page half back">
                 <div className="content">
                   <h1>Page 8</h1>
+                  <FontAwesomeIcon icon={faChartLine} size="3x" />
+                  <h3>I will multiply my value a hundredfold</h3>
                 </div>
               </div>
             </div>
@@ -110,12 +140,16 @@ export default function App() {
               <div className="page half front">
                 <div className="content">
                   <h1>Page 9</h1>
+                  <FontAwesomeIcon icon={faRunning} size="3x" />
+                  <h3>I will act now</h3>
                 </div>
               </div>
 
               <div className="page half back">
                 <div className="content">
                   <h1>Page 10</h1>
+                  <FontAwesomeIcon icon={faBrain} size="3x" />
+                  <h3>Guidance</h3>
                 </div>
               </div>
             </div>
@@ -151,15 +185,16 @@ export default function App() {
             </div>
 
             {/* Back of book */}
-            <div className="page book cover back book-page">
+            {/* <div className="page book cover back book-page"> */}
+            <div className="page book cover book-page">
               <div className="content">
                 <h1>End of Book</h1>
               </div>
             </div>
 
           </div>
-        </div>
-      </main>
-    </div>
+        </main>
+    //   </main>
+    // </div>
   );
 }
